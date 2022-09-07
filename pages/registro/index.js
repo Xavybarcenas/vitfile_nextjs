4839057c5cb5fc8b359ecdf26d1ae9ec8e225d5d
Xavybarcenas <57043767+Xavybarcenas@users.noreply.github.com>
2022-09-06 23:39:08 -0600

    cambios a la interfaz

--- pages/registro/index.js
+++ pages/registro/index.js
@@ -3,16 +3,21 @@ import Layout from "../../components/Layout";
 import styles from "/styles/registro/registro.module.css";
 import Link from "next/link";
 import Options from "/components/options";
+import Navbar from "../../components/nav";
 
 
 
 export default function Home() {
   return (
+    <div>
+      <Navbar/>
+    
     <div className={styles.container}>
       
       <Options/>
       <span className={styles.titulo}>El dashboard</span>
       
+      </div>
       </div>
   );
 }
