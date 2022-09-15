82aaa4fa60216bb2e3c88c38ef44da2565f4ab7d
Xavybarcenas <57043767+Xavybarcenas@users.noreply.github.com>
2022-09-15 14:39:48 -0600

    generando la interfaz

--- components/nav.js
+++ components/nav.js
@@ -1,13 +1,13 @@
 import Link from 'next/link'
-
+import styles from "../components/styles/nav.module.css";
 
 export default function nav() {
   return (
     <div>
-      <nav className="">
-        <ul className="flex item-stretch my-1 ">
-          <li className='block flex mx-2'><Link href="/dashboard"><a>Inicio</a></Link></li>
-          <li className='block flex'><Link href="#"><a>Configuracion</a></Link></li>
+      <nav className={styles.nav_container} >
+        <ul className={styles.ul}>
+          <li className={styles.li}><Link href="#"><a>Configuracion</a></Link></li>
+          <li className={styles.li}><Link href="/dashboard"><a>Inicio</a></Link></li>
         </ul>
       </nav>
     </div>
--- pages/dashboard/perfil.js
+++ pages/dashboard/perfil.js
@@ -1,12 +1,45 @@
 import Head from "next/head";
 import Layout from "/components/Layout";
 import Link from "next/link";
+import styles from "/styles/dashboard/perfil.module.css";
 
 export default function Perfil() {
-  return (<div>
-    <Layout title="Perfil">
-      <h1>Este es el perfil</h1>
-    </Layout>
-  </div>);
-  
+  return (
+    <div>
+      <Layout title="Perfil | Vitfile">
+        <h1>Ingresar Paciente</h1>
+        <div className={styles.form} id="perfil">
+          <form name="ingresar paciente">
+            <label className={styles.label}>
+              <span className={styles.span}>
+                Nombres
+              </span>
+              <input type="text" className={styles.input} />
+            </label>
+            <label className={styles.label}>
+              <span className={styles.span}>
+                Apellidos
+              </span>
+              <input type="text" className={styles.input} />
+            </label>
+            <label className={styles.label}>
+              <span className={styles.span}>
+                Edad
+              </span>
+              <input type="text" className={styles.input} />
+            </label>
+            <label className={styles.label}>
+              <span className={styles.span}>
+                Tipo de Sangre
+              </span>
+              <input type="text" className={styles.input} />
+            </label>
+            <button className={styles.button} formAction="perfil">
+              Guardar Cambios
+            </button>
+          </form>
+        </div>
+      </Layout>
+    </div>
+  );
 }
