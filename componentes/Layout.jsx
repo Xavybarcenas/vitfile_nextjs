961d017dcd473e2814a014e5dd358cbab6c65478
Xavybarcenas <57043767+Xavybarcenas@users.noreply.github.com>
2022-10-25 20:23:53 -0600

    Update Layout.jsx

--- componentes/Layout.jsx
+++ componentes/Layout.jsx
@@ -10,7 +10,7 @@ export default function layout({ title, children }) {
         <title>{title}</title>
       </Head>
       <Navbar />
-      <div className="fixed lg:left-[23.6%] lg:w-[76.6%] sm:left-0 sm:w-full scroll-auto h-screen">{children}</div>
+      <div className="absolute lg:left-[23.6%] lg:w-[76.6%] sm:left-0 sm:w-full scroll-auto h-screen">{children}</div>
 
       <aside>
         <Options />
